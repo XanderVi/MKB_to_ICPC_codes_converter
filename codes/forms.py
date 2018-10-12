81cbbcf6d131326027d5d0d2130f00a7e21da5b5
XanderVi <engineermexanic@gmail.com>
2018-10-12 13:03:53 +0300

    add to GitHub

---
+++ codes/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+'''
+class CodeForm(forms.Form):
+    code = forms.CharField(label='Код по МКБ-10', widget=forms.TextInput(attrs={'color':'white'}))
+
+    def clean_info(self):
+        data = self.cleaned_data['code']
+
+        # Remember to always return the cleaned data.
+        return data
+'''
